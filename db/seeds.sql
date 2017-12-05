b1048948dad28abe81e11130eb05d4bcae2583df
Jmorquecho92 <morquecho.jesus@yahoo.com>
2017-12-04 22:49:38 -0800

    updated files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers(burger_name) VALUES ('California Burger');
+INSERT INTO burgers(burger_name) VALUES ('Spicy Chicken Burger');
+INSERT INTO burgers(burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers(burger_name) VALUES ('Double Bacon Cheeseburger');
+INSERT INTO burgers(burger_name) VALUES ('Regular Hamburger');
